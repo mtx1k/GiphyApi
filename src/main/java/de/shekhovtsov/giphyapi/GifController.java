4e348569abc65488bf321a1a5e0ec1cb9ebce62c
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-06-04 16:19:05 +0200

    v2

--- src/main/java/de/shekhovtsov/giphyapi/GifController.java
+++ src/main/java/de/shekhovtsov/giphyapi/GifController.java
@@ -1,11 +1,29 @@
 package de.shekhovtsov.giphyapi;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class GifController {
     @FXML
+    VBox vbox;
+    @FXML
     protected void onRandomButtonClick() {
+        vbox.getChildren().clear();
+        ImageView imageView = new ImageView();
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(600);
 
+        String gifURL = new RandomGif().getGifURL();
+
+        if(gifURL != null) {
+            Image image = new Image(gifURL, true);
+            imageView.setImage(image);
+        }
+
+        vbox.getChildren().add(imageView);
     }
 }
---
+++ src/main/java/de/shekhovtsov/giphyapi/RandomGif.java
@@ -0,0 +1,53 @@
+package de.shekhovtsov.giphyapi;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RandomGif {
+    private static final String API_URL = "https://api.giphy.com/v1/gifs/random";
+    private static final String API_KEY = "SAUAZzwRTKgFX1XCPZJJyA5o55zV4vmn"; // Replace with your Giphy API key
+    private static final String TAG = "funny";
+
+    public RandomGif() {
+
+    }
+
+    public String getGifURL() {
+
+        return fetchRandomGifUrl();
+    }
+
+    private String fetchRandomGifUrl() {
+        try {
+            URL url = new URL(API_URL + "?api_key=" + API_KEY + "&tag=" + TAG);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                GifObject gifObject = objectMapper.readValue(response.toString(), GifObject.class);
+                return gifObject.data().images().original().url();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
